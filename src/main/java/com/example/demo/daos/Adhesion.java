8abbe196bf5b32f83ceb8f4cf005bc8c4054ce76
NadirAmmor <nadir.ammor09@gmail.com>
2021-11-20 12:06:23 +0100

    initial project

---
+++ src/main/java/com/example/demo/daos/Adhesion.java
@@ -0,0 +1,61 @@
+package com.example.demo.daos;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+public class Adhesion implements Serializable {
+
+    EtudiantClubKey  id;
+
+    Etudiant etudiant;
+
+    Club club;
+
+
+    private String statut;
+
+    private Date dateAdhesion;
+
+    //getters
+
+    public EtudiantClubKey getId() {
+        return id;
+    }
+
+    public void setId(EtudiantClubKey id) {
+        this.id = id;
+    }
+
+    public Etudiant getEtudiant() {
+        return etudiant;
+    }
+
+    public void setEtudiant(Etudiant etudiant) {
+        this.etudiant = etudiant;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public Date getDateAdhesion() {
+        return dateAdhesion;
+    }
+
+    public void setDateAdhesion(Date dateAdhesion) {
+        this.dateAdhesion = dateAdhesion;
+    }
+}
